03eb4cda823b035afc1b8e955cf2b5590ad1426b
Yue Li <michaelli0914@outlook.com>
2023-12-07 22:39:13 +0000

    add query

--- mylib/query.py
+++ mylib/query.py
@@ -2,12 +2,29 @@ import sqlite3
 from prettytable import PrettyTable
 
 def Query1(main_genre):
-    '''Query the top 5 rows of the GroceryDB table'''
     conn = sqlite3.connect("MovieDB.db")
     cursor = conn.cursor()
     cursor.execute("SELECT Movie_Title, Year, Director, Rating, main_genre FROM MovieDB WHERE main_genre='{main_genre}'")
 
     column_names = ["Movie Title", "Year", "Director", "Rating", "main_genre"]
+
+    table = PrettyTable(column_names) # initialize table with column names
+
+    for row in cursor.fetchall():
+        table.add_row(row) # fetch rows and add them to the table
+
+    print(table)
+
+    conn.close()
+
+    return "Success"
+
+def Query2(director):
+    conn = sqlite3.connect("MovieDB.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT Movie_Title, Year, Director, Rating FROM MovieDB WHERE Director='{director}' ORDER BY Rating DESC")
+
+    column_names = ["Movie Title", "Year", "Director", "Rating"]
 
     table = PrettyTable(column_names) # initialize table with column names
 
