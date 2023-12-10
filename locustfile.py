271d2f2866a67039c3ffa0b4e13a797e175003b8
Yue Li <michaelli0914@outlook.com>
2023-12-10 05:48:35 +0000

    update locustfile

--- locustfile.py
+++ locustfile.py
@@ -5,9 +5,5 @@ class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def load_grocery_info(self):
-        self.client.get("/grocery_info/coffee")
-
-    @task
-    def load_count_info(self):
-        self.client.get("/product_count/20")
+    def task1(self):
+        self.client.get("/")
